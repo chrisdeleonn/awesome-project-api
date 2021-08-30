ff3ee17050f4f5bf14b90ea6dd8fff75bb78d134
Chris De Leon <cdeleon.ca91@gmail.com>
2021-08-30 14:08:53 -0400

    refactored

--- src/countrollers/carsController.js
+++ src/countrollers/carsController.js
@@ -1,7 +1,6 @@
 const logHistorySchema = require("../models/carsModel");
 
 exports.createLogHistory = (req, res) => {
-	console.log("inside the controller block.", req.body);
 	new logHistorySchema(req.body)
 		.save()
 		.then(() => res.status(200).send("created entry."))
@@ -9,7 +8,6 @@ exports.createLogHistory = (req, res) => {
 };
 
 exports.getLogHistory = (req, res) => {
-	console.log("inside the history block.");
 	logHistorySchema.find({}).then((data) => {
 		res.send({ data });
 	});
